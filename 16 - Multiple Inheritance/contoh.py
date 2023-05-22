05d12731cb5669a6074e402ebdd8b7732cda68a6
wicaksonoindra <wicaksonoindra66@gmail.com>
2023-05-22 18:06:20 +0700

    eps 16

---
+++ 16 - Multiple Inheritance/contoh.py
@@ -0,0 +1,27 @@
+class Team:
+    def setTeam(self,team):
+        self.team = team
+
+    def showTeam(self):
+        print(self.team)
+
+class Tipe_Hero:
+    def setTipe(self,tipe):
+        self.tipe = tipe
+
+    def showTipe(self):
+        print(self.tipe)
+
+
+class Hero(Team,Tipe_Hero):
+
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
+
+Ucup = Hero("ucup",100)
+Ucup.setTeam("banteng")
+Ucup.setTipe("boomer")
+
+Ucup.showTeam()
+Ucup.showTipe()
